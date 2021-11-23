53c24806e7e6bdefb13850a4dcfe67fa042c7b90
suryanshshukla10 <suryanshshukla@outlook.com>
2021-11-23 10:20:42 -0500

    remove unnecessary files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CIS-PA3
 Programming Assignment 3
+
+#Run 
+type the following command 
+'
+python3 pa3.py
+'
--- ciscode/faceClosestPt.py
+++
@@ -1,7 +0,0 @@
-import numpy
-from numpy import dot
-from math import sqrt
-
-
-def faceClosestPt():
-    fnorm  # normal to face
